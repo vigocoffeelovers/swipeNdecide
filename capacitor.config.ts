1172a97cef62b54123edc2bce051d0dd0143c60f
RoiMP <roimartinezportela@gmail.com>
2023-04-18 17:37:45 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'swipe.n.decide',
+  appName: 'swipe-n-decide',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
