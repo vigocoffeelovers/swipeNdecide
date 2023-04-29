20a2060d629d72853fdd2c2fbed4b7adf8853b4d
Sergio Arcay <arcay.sergio@gmail.com>
2023-04-29 21:36:36 +0200

    Use swiper package

--- src/app/pages/swiper/swiper.page.ts
+++ src/app/pages/swiper/swiper.page.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { CardsComponentsModule } from "../../components/components.module";
+import { register } from 'swiper/element/bundle';
+register();
 
 @Component({
     selector: 'app-swiper',
     templateUrl: './swiper.page.html',
     styleUrls: ['./swiper.page.scss'],
     standalone: true,
-    imports: [IonicModule, CommonModule, CardsComponentsModule]
+    imports: [IonicModule, CommonModule, CardsComponentsModule],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SwiperPage implements OnInit {
 
     constructor() { }
 
-    ngOnInit() {
-    }
+    ngOnInit() { }
 
 }
