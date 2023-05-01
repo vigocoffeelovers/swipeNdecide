0626c2e612561b7b1cea32976a12389375b0891f
Sergio Arcay <arcay.sergio@gmail.com>
2023-05-01 20:34:55 +0200

    Fix spec

--- src/app/pages/swiper/swiper.page.spec.ts
+++ src/app/pages/swiper/swiper.page.spec.ts
@@ -1,11 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SwiperPage } from './swiper.page';
+import { waitForAsync } from '@angular/core/testing';
 
 describe('SwiperPage', () => {
   let component: SwiperPage;
   let fixture: ComponentFixture<SwiperPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(SwiperPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
