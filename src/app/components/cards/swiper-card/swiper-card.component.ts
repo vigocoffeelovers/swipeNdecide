d58ff4b6b5c684974af9c4db0779f797ea47a6dc
Sergio Arcay <arcay.sergio@gmail.com>
2023-05-10 10:13:55 +0200

    Allow swipe to both sides

--- src/app/components/cards/swiper-card/swiper-card.component.ts
+++ src/app/components/cards/swiper-card/swiper-card.component.ts
@@ -9,6 +9,12 @@ export class SwiperCardComponent implements OnInit {
   @Input() imageSrc: string | undefined;
   @Input() title: string | undefined;
   @Input() tags: string[] | undefined;
+  @Input() styleCard: string | undefined;  // invisible | visible | finish
+  /* Invisible: the first card. Invisible. The user should never reach it. It is used to allow the user to swipe to the left.
+   * Visible: normal cards. The user swipes to the right to vote for these card.
+   * Finish: the last card. It contains a message to the user to inform him that he has finished the voting.
+   */
+
 
   constructor() { }
 
--- src/app/pages/swiper/swiper.page.ts
+++ src/app/pages/swiper/swiper.page.ts
@@ -15,6 +15,14 @@ register();
 })
 export class SwiperPage implements OnInit, AfterViewInit {
 
+    options = [
+        { imageSrc: "assets/images/default-card-image.png", title: "Option 1", tags: ["tag1", "tag2", "tag3"] },
+        { imageSrc: "assets/images/default-card-image.png", title: "Option 2", tags: ["tag1", "tag2", "tag3"] },
+        { imageSrc: "assets/images/default-card-image.png", title: "Option 3", tags: ["tag1", "tag2", "tag3"] },
+        { imageSrc: "assets/images/default-card-image.png", title: "Option 4", tags: ["tag1", "tag2", "tag3"] },
+        { imageSrc: "assets/images/default-card-image.png", title: "Option 5", tags: ["tag1", "tag2", "tag3"] },
+    ];
+
     constructor() { }
 
     ngOnInit() { }
@@ -25,24 +33,22 @@ export class SwiperPage implements OnInit, AfterViewInit {
 
     initializeSwiper() {
         const swiperEl = document.querySelector('swiper-container');
-        swiperEl!.addEventListener('swiper-slidenexttransitionend', (event: any) => this.onSlideNextTransitionStart(event));
-        swiperEl!.addEventListener('swiper-slideprevtransitionend', (event: any) => this.onSlidePrevTransitionEnd(event));
+        swiperEl!.addEventListener('swiper-slidenexttransitionend', (event: any) => this.onSlideNextTransition(event, true));
+        swiperEl!.addEventListener('swiper-slideprevtransitionend', (event: any) => this.onSlideNextTransition(event, false));
     }
 
     /* Swiper EVENTS */
 
     /**
-     * Event will be fired after animation to next slide
+     * Event will be fired after animation to another slide (next or previous).
+     *
+     * @param event Event object (event.target.swiper is Swiper instance of this slider).
+     * @param next If true then the transition was in next direction, otherwise - in previous direction.
      */
-    onSlideNextTransitionStart(event: any) {
-        console.log('onSlideNextTransitionStart');
-    }
-
-    /**
-     * Event will be fired previous animation to next slide
-     */
-    onSlidePrevTransitionEnd(event: any) {
-        console.log('onSlidePrevTransitionEnd');
+    onSlideNextTransition(event: any, next: boolean) {
+        /* Remove the first slide, already voted by the user */
+        event.target.swiper.removeSlide(1);
+        event.target.swiper.slideTo(1, 0, false);
     }
 
 }
